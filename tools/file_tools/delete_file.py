e875dd7a1532f7eee0e5cbba563e505acc76114d
zhaomingxing.93 <zhaomingxing.93@bytedance.com>
2021-06-05 16:45:52 +0800

    [feat]新增删除特定目录文件的脚本

---
+++ tools/file_tools/delete_file.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+===============================================================================
+author: 赵明星
+desc:   删除指定目录下的指定文件名的所有文件
+===============================================================================
+"""
+
+import os
+import shutil
+
+
+def delete_output(work_dir):
+    for parent, dir_names, _ in os.walk(work_dir, followlinks=False):
+        for dir_name in dir_names:
+            if dir_name == "output":
+                print(os.path.join(parent, dir_name))
+                shutil.rmtree(os.path.join(parent, dir_name))
+                continue
+            delete_output(os.path.join(parent, dir_name))
+
+
+if __name__ == "__main__":
+    delete_output(os.getcwd())
