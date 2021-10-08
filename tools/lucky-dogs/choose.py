e8603669e89fd8a2211de6e2913e5fd5fbdc9435
zhaomingxing.93 <zhaomingxing.93@bytedance.com>
2021-10-08 11:52:54 +0800

    [feat]添加产生幸运数字的代码

--- tools/lucky-dogs/choose.py
+++ tools/lucky-dogs/choose.py
@@ -14,22 +14,16 @@ import numpy as np
 def choose_lucky_dog(candidates, total_dog_num):
     dogs = []
     while len(dogs) < total_dog_num:
-        dog_candidate = np.random.randint(1, len(num_name_dict))
+        dog_candidate = np.random.randint(1, len(candidates))
         if dog_candidate not in dogs:
             dogs.append(dog_candidate)
     return [candidates[lucky_dog] for lucky_dog in dogs]
 
 
+def choose_a_luck_num(low, high):
+    return np.random.randint(low, high)
+
+
 if __name__ == "__main__":
-    num_name_dict = {
-                1: "赵明星",
-                2: "陈琨",
-                3: "蒲黎明",
-                4: "张庆恒",
-                5: "康家鹏",
-                6: "郭明",
-                7: "肖宗阳",
-                8: "王刘"
-                }
-    print(choose_lucky_dog(num_name_dict, 5))
+    print(choose_a_luck_num(50, 200))
 
