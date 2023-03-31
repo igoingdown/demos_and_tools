93fb85a30bd71f9c8c6490c408b574d1f643f6b6
zhaomingxing.93 <zhaomingxing.93@bytedance.com>
2023-03-31 17:54:31 +0800

    [fix]新增写入csv 功能
    Change-Id: I719697ed1caf039e4a688f3237870703b88de575

--- tools/loan_calculator/main.py
+++ tools/loan_calculator/main.py
@@ -2,6 +2,7 @@
 
 import sys
 import getopt
+import csv
 
 '''
 等额本息计算公式
@@ -40,15 +41,37 @@ def commercial_loan(loan_principal, month_num):
     return cur_month_principal, cur_month_interest
 
 
+def format_money(num):
+    return round(num, 2)
+
 def public_savings_loan():
     return 1603
 
 
-def print_plan(principal, month):
+def get_loan_pay_plan(principal, month):
+    cur_month_public_savings_loan = public_savings_loan()
+    plan = []
+    plan_header = ["month", "cur_month_total_fee", "cur_month_principal", "cur_month_interest",
+                   "cur_month_commercial_loan", "cur_month_public_savings_loan",
+                   "origin_principal", "left_loan_principal"]
+    plan.append(plan_header)
     while month > 0:
         cur_month_principal, cur_month_interest = commercial_loan(principal, month)
+        origin_principal = principal
         month -= 1
         principal -= cur_month_principal
+        left_loan_principal = principal
+        cur_total_debt = cur_month_interest + cur_month_principal+cur_month_public_savings_loan
+        cur_total_commercial_debt = cur_month_interest + cur_month_principal
+        plan.append([300-month,
+                     format_money(cur_total_debt),
+                     format_money(cur_month_principal),
+                     format_money(cur_month_interest),
+                     format_money(cur_total_commercial_debt),
+                     format_money(cur_month_public_savings_loan),
+                     format_money(origin_principal),
+                     format_money(left_loan_principal)])
+    return plan
 
 
 def parse_params():
@@ -68,6 +91,13 @@ def parse_params():
     return int(principal_str), int(month_num_str)
 
 
+def write_csv(data):
+    # 打开 csv 文件并写入数据
+    with open('loan_plan.csv', mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(data)
+
+
 def main():
     principal, month = parse_params()
     cur_month_principal, cur_month_interest = commercial_loan(principal, month)
@@ -78,7 +108,8 @@ def main():
                  cur_month_interest,
                  cur_month_interest + cur_month_principal,
                  cur_month_public_savings_loan))
-    print_plan(principal, month)
+    plan = get_loan_pay_plan(principal, month)
+    write_csv(plan)
 
 
 if __name__ == '__main__':
