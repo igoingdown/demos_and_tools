fbbfef26aa157f13f4559ee62316510e060aa6de
zhaomingxing.93 <zhaomingxing.93@bytedance.com>
2023-09-02 22:20:24 +0800

    [fix]使用 argparse 简化代码
    Change-Id: Id9683ece5f12ea5d1bf398f374032a2f81a3ddfd

--- tools/loan_calculator/main.py
+++ tools/loan_calculator/main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-import sys
-import getopt
 import csv
+import argparse
+
 
 '''
 等额本息计算公式
@@ -17,21 +17,20 @@ import csv
 总利息=还款月数×每月月供额-贷款本金
 '''
 
-LPR = 0.043
-BP_Plus = 55*0.0001
 
-
-def commercial_loan(loan_principal, month_num):
+def commercial_loan(loan_principal, month_num, lpr, bp_plus):
     """ calculate multi num about a loan depend on args.
 
     Args:
         loan_principal: The principal of a loan.
         month_num: Num of months of the loan.
+        lpr: lpr of a country.
+        bp_plus: base point add to lpr.
 
     Returns:
         A loan detail struct.
     """
-    yearly_interest_rate = LPR + BP_Plus
+    yearly_interest_rate = lpr + bp_plus
     monthly_interest_rate = yearly_interest_rate / 12
     cur_month_fee = loan_principal * monthly_interest_rate * ((1 + monthly_interest_rate) ** month_num) / \
                       ((1 + monthly_interest_rate) ** month_num - 1)
@@ -48,7 +47,7 @@ def public_savings_loan():
     return 1603
 
 
-def get_loan_pay_plan(principal, month):
+def get_loan_pay_plan(principal, month,lpr, bp_plus):
     cur_month_public_savings_loan = public_savings_loan()
     plan = []
     plan_header = ["month", "cur_month_total_fee", "cur_month_principal", "cur_month_interest",
@@ -56,7 +55,7 @@ def get_loan_pay_plan(principal, month):
                    "origin_principal", "left_loan_principal"]
     plan.append(plan_header)
     while month > 0:
-        cur_month_principal, cur_month_interest = commercial_loan(principal, month)
+        cur_month_principal, cur_month_interest = commercial_loan(principal, month, lpr, bp_plus)
         origin_principal = principal
         month -= 1
         principal -= cur_month_principal
@@ -75,20 +74,13 @@ def get_loan_pay_plan(principal, month):
 
 
 def parse_params():
-    principal_str, month_num_str = "", ""
-    argv = sys.argv[1:]
-    try:
-        opts, args = getopt.getopt(argv, "p:m:",
-                                   ["principal=", "month="])  # 长选项模式
-    except:
-        print("Error")
-        return int(principal_str), int(month_num_str)
-    for opt, arg in opts:
-        if opt in ['-p', '--principal']:
-            principal_str = arg
-        elif opt in ['-m', '--month']:
-            month_num_str = arg
-    return int(principal_str), int(month_num_str)
+    parser = argparse.ArgumentParser(description="脚本描述信息")
+    # 添加参数
+    parser.add_argument("-p", "--principal", help="贷款总金额", type=int, default=4000000)
+    parser.add_argument("-m", "--month", help="还款月数", type=int, default=300)
+    parser.add_argument("-r", "--rate", help="5 年期贷款LPR", type=float, default=0.043)
+    parser.add_argument("-b", "--base_point", help="额外加息 BP", type=float, default=0.0055)
+    return parser.parse_args()
 
 
 def write_csv(data):
@@ -99,8 +91,9 @@ def write_csv(data):
 
 
 def main():
-    principal, month = parse_params()
-    cur_month_principal, cur_month_interest = commercial_loan(principal, month)
+    args = parse_params()
+    loan_principal, month_num, lpr, bp_plus = args.principal, args.month, args.rate,args.base_point
+    cur_month_principal, cur_month_interest = commercial_loan(loan_principal, month_num, lpr, bp_plus)
     cur_month_public_savings_loan = public_savings_loan()
     print("total_fee:{}, cur_month_principal:{}, cur_month_interest:{}, commercial_loan:{}, public_savings_loan:{}".
           format(cur_month_principal + cur_month_interest + cur_month_public_savings_loan,
@@ -108,7 +101,7 @@ def main():
                  cur_month_interest,
                  cur_month_interest + cur_month_principal,
                  cur_month_public_savings_loan))
-    plan = get_loan_pay_plan(principal, month)
+    plan = get_loan_pay_plan(loan_principal, month_num, lpr, bp_plus)
     write_csv(plan)
 
 
