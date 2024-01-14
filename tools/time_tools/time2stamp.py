3669e1503f9c2f4b77ccba03ee4aac6d20d41e6e
zhaomingxing.93 <zhaomingxing.93@bytedance.com>
2024-01-14 10:36:42 +0800

    [fix]兼容其他格式
    Change-Id: I4cbdda8ed9b2699ac0a976bbab1ece77e7aced87

--- tools/time_tools/time2stamp.py
+++ tools/time_tools/time2stamp.py
@@ -4,9 +4,14 @@ import sys
 
 
 def main(argv):
-    print(argv)
-    time_stamp = datetime.datetime.timestamp(datetime.datetime.strptime(argv[0],"%Y-%m-%d %H:%M:%S"))
-    print(time_stamp)
+    #print(argv)
+    try:
+        time_stamp = datetime.datetime.timestamp(datetime.datetime.strptime(argv[0],"%Y-%m-%d %H:%M:%S"))
+        print(int(time_stamp))
+    except Exception as e:
+        time_stamp = datetime.datetime.timestamp(datetime.datetime.strptime(argv[0],"%Y/%m/%d %H:%M:%S"))
+        print(int(time_stamp))
+
 
 
 if __name__ == "__main__":
