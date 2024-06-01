0a1633342c8b5ce13554b8ec74426b14af8b402d
zhaomingxing.93 <zhaomingxing.93@bytedance.com>
2024-06-01 18:20:01 +0800

    [fix]更新帖子
    Change-Id: I4dc0cefaeac71b8f9839062ca6c0db6117d78a9c

--- tools/bbs_crawler/trade_multi_reply.sh
+++ tools/bbs_crawler/trade_multi_reply.sh
@@ -2,5 +2,5 @@
 
 cd $HOME/github/python_demo_and_tool/
 
-/usr/bin/python3 $HOME/github/python_demo_and_tool/tools/bbs_crawler/trade_reply.py -t 2086477
-python3 tools/bbs_crawler/trade_reply.py
+/usr/bin/python3 $HOME/github/python_demo_and_tool/tools/bbs_crawler/trade_reply.py -t 2090110
+
